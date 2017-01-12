08beee6498fe7b2ec289ca18fb889873942501d3
Ryan Kollar <ryan.kollar@amtrustgroup.com>
2017-01-12 08:34:56 -0500

    Can now purchase a product

--- ts/vendingMachine.ts
+++ ts/vendingMachine.ts
@@ -21,8 +21,11 @@ class Cell {
 
 class VendingMachine {    
     private paid = ko.observable(0);
+    selectedCell = ko.observable(new Cell(new CocaCola()));
     cells = ko.observableArray([]);
     acceptedCoins: Quarter[] = [new Quarter()];
+    canPay = ko.pureComputed(() => this.paid() - 
+                    this.selectedCell().product.price >= 0);
 
     set size(givenSize: VendingMachineSize) {
         this.cells([]);
@@ -33,10 +36,28 @@ class VendingMachine {
         }
     }
 
+    select = (cell: Cell): void => {
+        cell.sold(false);
+        this.selectedCell(cell);
+    }
+
     acceptCoin = (coin:Quarter): void => {
         let oldTotal = this.paid();
         this.paid(oldTotal + coin.Value);
 
         console.log(oldTotal);
+    }
+
+    pay = (): void => {
+        if (this.selectedCell().stock() == 0) {
+            alert("I'm sorry, we're out of them!");
+            return;
+        }
+
+        let currentPaid = this.paid();
+        this.paid(Math.round(((currentPaid - this.selectedCell().product.price) *100))/100);
+        let currentStock = this.selectedCell().stock();
+        this.selectedCell().stock(currentStock - 1);
+        this.selectedCell().sold(true);
     }
 }
